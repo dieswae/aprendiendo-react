9362bcd10e0a566e1715a848765bf43b61e72fde
dieswae <diegooare11@gmail.com>
2024-05-15 22:54:24 -0300

    juego funcional, comenzando a hacer el reset

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -10,17 +10,66 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [8, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 8],
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
-
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // no actualizamos estas posición
+    // si ya tiene algo
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
 
   return (
     <main className="board">
@@ -31,18 +80,48 @@ function App() {
             return (
               <Square
                 key={index}
-                index={index}>
-                  
-                </Square>
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
       </section>
 
       <section className="turn">
-        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó'
+                }
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
